b6f4447c3ecd50df3f2902b46363fc3342c5f88a
Clark Van Oyen <clark@countable.ca>
2015-02-21 19:32:19 -0800

    data updates, bugfixes

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var indicators = {
 
 };
 
+
+var isIndicatorInverted = function(indicator) {
+    return (indicator || current_indicator) == 'Bike helmet' || (indicator || current_indicator) == 'Flu shot';
+};
+
 HR.features.forEach(function(feature){
   for (var k in feature.properties) {
     if (feature.properties.hasOwnProperty(k)) {
+      if (isIndicatorInverted(k) && feature.properties[k]) feature.properties[k] = 100 - feature.properties[k];
       indicators[k] = indicators[k] || {};
       if (feature.properties[k] !== null) {
         indicators[k].max = Math.max(indicators[k].max || 0, feature.properties[k]);
@@ -36,7 +42,12 @@ resetStyles = function(){
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiZWNvdHJ1c3QiLCJhIjoibGo4TG5nOCJ9.QJnT2dgjL4_4EA7WlK8Zkw';
 
-var map = L.mapbox.map('map', 'examples.3hqcl3di')
+var map = L.mapbox.map('map', 'ecotrust.405061ba',
+{
+  legendControl: {
+    position:'topright'
+  }
+})
   .setView([67.8, -96], 4);
 
 var popup = new L.Popup({ autoPan: false });
@@ -55,14 +66,16 @@ function getStyle(feature) {
 var legendColors = ["#ffffcc",
   "#ffeda0", "#fed976", "#feb24c","#fd8d3c",
   "#fc4e2a","#e31a1c", "#b10026"];
-// get color depending on population density value
-function getColor(d) {
-    if (d === null) return "#808080";
-    var bounds = indicators[current_indicator];
-    return legendColors[Math.floor(((d - bounds.min) / (bounds.max-bounds.min)) * legendColors.length)];
+function getColor(d, indicator) {
+    indicator = indicator || current_indicator;
+    if (d === null || typeof d === 'undefined') return "#808080";
+    var bounds = indicators[indicator];
+    var index = Math.floor(((d - bounds.min) / (bounds.max-bounds.min)) * legendColors.length * .999)
+    var color = legendColors[index];
+    return color;
 }
 
-function onEachFeature(feature, layer) {
+var attachLayerEvents = function(feature, layer) {
     layer.on({
         mousemove: mousemove,
         mouseout: mouseout,
@@ -72,12 +85,23 @@ function onEachFeature(feature, layer) {
 
 var closeTooltip;
 
+var indicatorDisplayName = function(layer){
+    var val = current_indicator;
+    if (isIndicatorInverted()) val = 'Missing ' + val;
+    return val
+};
+
+var indicatorDisplayData = function(layer){
+    var val = layer.feature.properties[current_indicator] || '(no data)';
+    return val
+};
+
 function mousemove(e) {
     var layer = e.target;
 
     popup.setLatLng(e.latlng);
     popup.setContent('<div class="marker-title">' + layer.feature.properties.region + '</div>' +
-        layer.feature.properties[current_indicator] + ' ' + current_indicator + ' prevalence');
+        indicatorDisplayData(layer) + ' ' + current_indicator + ' prevalence');
 
     if (!popup._map) popup.openOn(map);
     window.clearTimeout(closeTooltip);
@@ -101,16 +125,27 @@ function mouseout(e) {
     }, 100);
 }
 
+var selectedLayer;
 function zoomToFeature(e) {
+  selectedLayer = e.target;
     map.fitBounds(e.target.getBounds());
     var props = e.target.feature.properties;
-    var report = '<h1>' + props.region + '</h1>'
+    var $report = $('<h1>' + props.region + '</h1>' + $("#report_template").html());
     for (k in props){
       if (props.hasOwnProperty(k)) {
-        report += k + ' : ' + (props[k] || '(no data)') + '<br>'
+        $item = $report.find('[data-indicator="'+ k + '"]');
+        if (props[k] && $item.length) {
+          $item.find('h3').text(props[k] + '% in your area.');
+          console.log(getColor(props[k], k), k);
+          $item.css("border-left-color", getColor(props[k], k));
+        } else {
+          $item.hide();
+        }
       }
     };
-    $("#report").html(report);
+    $("#report").empty().append($report);
+    $("#toggleview").show();
+    $("#toggleview .region").text("Now living in " + props.region);
 }
 
 
@@ -121,47 +156,75 @@ function getLegendHTML() {
   for (var i = 0; i < legendColors.length; i++) {
     from = bounds.min + (bounds.max-bounds.min)*i/legendColors.length;
     to = bounds.min + (bounds.max-bounds.min)*(i+1)/legendColors.length;
-    color = getColor(to - .1);
     labels.push(
       '<li><span class="swatch" style="background:' + getColor(to - .1) + '"></span> ' +
       from.toFixed(1) + (to ? '&ndash;' + to.toFixed(1) : '+')) + '</li>';
   }
 
-  return '<span>Percent </span><span id="current_indicator">' + current_indicator + '</span><ul>' + labels.join('') + '</ul>';
+  return '<span>Percent </span><span id="current_indicator">' + indicatorDisplayName() + '</span><ul>' + labels.join('') + '</ul>';
 }
 
 var pickPrimaryView = function(which){
-  $(".primary").css('z-index',0);
-  $("#"+which).css('z-index',100);
+  $(".primary").css('display','none');
+  $("#"+which).css('display','block');
+  $("#toggle .item").show();
+  $('[data-show="'+which+'"]').hide();
 };
 
 var pickSubmenu = function(which){
+  $("#menu img").swapActiveImg(false);
   $(".submenu").css('bottom','-100px');
-  $("#"+which).css('bottom','');
+  $("#"+which).css('bottom','80px');
+  $img = $('[data-show="'+which+'"] img');
+  $img.swapActiveImg(true);
 };
 
+var current_indicator;
+var toggleIndicatorIcon = function(state){
+  if (!current_indicator) return;
+  var $img = $('[data-indicator="'+current_indicator+'"]').find("img");
+  $img.swapActiveImg(state);
+};
+$.fn.swapActiveImg = function(state) {
+  $(this).each(function(){
+    $img = $(this);
+    if (state) {
+      $img.addClass('active');
+    } else {
+      $img.removeClass('active');
+    }
+    $img.attr('src', $img.attr('src').replace("icons/" + (state ? 'i' : 'a'), "icons/" + (state ? 'a' : 'i')));
+  });
+}
+
 var pickIndicator = function(which, initial){
+  toggleIndicatorIcon(false);
   current_indicator = which;
   if (!initial) resetStyles();
   if (legendHTML) map.legendControl.removeLegend(legendHTML);
   legendHTML = getLegendHTML();
-  map.legendControl.addLegend(legendHTML);
+  map.legendControl.addLegend(legendHTML,{position: 'topright'});
+  toggleIndicatorIcon(true);
 };
 
-$('#toggle').click(function(e){
-  var which = $(e.target).attr('data-show');
-  pickPrimaryView(which);
-});
-
 $('#menu').click(function(e){
-  var which = $(e.target).attr('data-show');
+  var which = $(e.target).parents('.item').attr('data-show');
   pickSubmenu(which);
 });
 
 $('.submenu').click(function(e){
-  var which = $(e.target).attr('data-indicator');
+  var which = $(e.target).parents('.item').attr('data-indicator');
   pickIndicator(which);
+});
 
+$('#toggleview').click(function(){
+  $('#map').css('display','none');
+  $('#report').css('display','block');
+});
+
+$('body').on( "click", "#close_report", function(){
+  $('#report').css('display','none');
+  $('#map').css('display','block');
 });
 
 var legendHTML = '';
@@ -172,6 +235,6 @@ pickIndicator('Flu shot', true);
 
 var hrLayer = L.geoJson(HR,  {
     style: getStyle,
-    onEachFeature: onEachFeature
+    onEachFeature: attachLayerEvents
 });
 hrLayer.addTo(map);
